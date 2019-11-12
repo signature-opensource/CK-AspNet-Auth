4c95ecc3ccf5561b45ff6c1dfdbee387f94bb9f4
Hugo Zammit <hugo.zammit@signature-code.com>
2019-11-12 19:03:21 +0100

    [FIX] Improve local storage detection.

--- Clients/webfrontauth/ts/src/AuthServiceConfiguration.ts
+++ Clients/webfrontauth/ts/src/AuthServiceConfiguration.ts
@@ -8,11 +8,11 @@ export class AuthServiceConfiguration {
     public get localStoragePersistence(): ILocalStoragePersistence { return this._localStoragePersistence; }
 
     constructor(config: IAuthServiceConfiguration) {
-        this._identityServerEndPoint = this.getUrlFromEndPoint(config.identityEndPoint);
-        this._localStoragePersistence = this.ensureLocalStoragePersistence(config.localStoragePersistence);
+        this._identityServerEndPoint = AuthServiceConfiguration.getUrlFromEndPoint(config.identityEndPoint);
+        this._localStoragePersistence = AuthServiceConfiguration.ensureLocalStoragePersistence(config.localStoragePersistence);
     }
 
-    private getUrlFromEndPoint(endPoint: IEndPoint): string {
+    private static getUrlFromEndPoint(endPoint: IEndPoint): string {
         if (!endPoint.hostname) { return '/'; }
         const isHttps = !endPoint.disableSsl;
         const hostnameAndPort = endPoint.port !== undefined && endPoint.port !== null && !this.isDefaultPort(isHttps, endPoint.port)
@@ -24,20 +24,37 @@ export class AuthServiceConfiguration {
             : '/';
     }
 
-    private isDefaultPort(isHttps: boolean, portNumber: number): boolean {
+    private static isDefaultPort(isHttps: boolean, portNumber: number): boolean {
         return isHttps ? portNumber === 443 : portNumber === 80;
     }
 
-    private ensureLocalStoragePersistence(localStoragePersistence: ILocalStoragePersistence): ILocalStoragePersistence {
-        if (!!localStoragePersistence) {
-            const { onBasicLogin, onRefresh, onStartLogin, onUnsafeDirectLogin } = localStoragePersistence;
-            if (!(onBasicLogin || onRefresh || onStartLogin || onUnsafeDirectLogin)) { return localStoragePersistence; }
-        }
+    private static ensureLocalStoragePersistence(localStoragePersistence?: ILocalStoragePersistence): ILocalStoragePersistence {
+        return this.storageAvailable('localStorage')
+            ? localStoragePersistence || { onBasicLogin: true, onRefresh: true, onStartLogin: true, onUnsafeDirectLogin: true }
+            : { onBasicLogin: false, onRefresh: false, onStartLogin: false, onUnsafeDirectLogin: false };
+    }
 
-        if (!(typeof (window) !== 'undefined' && window.localStorage)) {
-            return { onBasicLogin: false, onRefresh: false, onStartLogin: false, onUnsafeDirectLogin: false };
-        }
+    /**
+     * Detects whether the localStorage is available.
+     * Reference: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Feature-detecting_localStorage
+     * @param storageType Storage type, either 'localStorage' or 'sessionStorage'
+     */
+    private static storageAvailable(storageType: 'localStorage' | 'sessionStorage'): boolean {
+        let storage: Storage;
+        try {
+            if (typeof (window) === 'undefined')
+                return false;
 
-        return localStoragePersistence || { onBasicLogin: true, onRefresh: true, onStartLogin: true, onUnsafeDirectLogin: true };
+            storage = window[storageType];
+            const key = '__storage_test__';
+            storage.setItem(key, key);
+            storage.removeItem(key);
+            return true;
+        }
+        catch (e) {
+            return e instanceof DOMException
+                && (e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+                && (storage && storage.length !== 0);
+        }
     }
 }
