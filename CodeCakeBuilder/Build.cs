208f19ac7c353b33c0b7830c7f03d3e2cfc3a035
Olivier Spinelli <olivier.spinelli@signature-code.com>
2020-05-07 17:39:52 +0200

    Applied settings.  (...) Upgraded dependencies.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -1,3 +1,9 @@
+using Cake.Npm;
+using Cake.Npm.RunScript;
+using Cake.Npm;
+using Cake.Npm.RunScript;
+using Cake.Npm;
+using Cake.Npm.RunScript;
 using Cake.Common.IO;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Build;
@@ -20,8 +26,7 @@ namespace CodeCake
         {
             Cake.Log.Verbosity = Verbosity.Diagnostic;
 
-            SimpleRepositoryInfo gitInfo = Cake.GetSimpleRepositoryInfo();
-            StandardGlobalInfo globalInfo = CreateStandardGlobalInfo( gitInfo )
+            StandardGlobalInfo globalInfo = CreateStandardGlobalInfo()
                                                 .AddDotnet()
                                                 .AddNPM()
                                                 .SetCIBuildTag();
@@ -65,7 +70,7 @@ namespace CodeCake
                 } );
 
             Task( "Create-Packages" )
-                .WithCriteria( () => gitInfo.IsValid )
+                .WithCriteria( () => globalInfo.IsValid )
                 .IsDependentOn( "Unit-Testing" )
                 .Does( () =>
                  {
@@ -75,7 +80,7 @@ namespace CodeCake
 
 
             Task( "Push-Packages" )
-                .WithCriteria( () => gitInfo.IsValid )
+                .WithCriteria( () => globalInfo.IsValid )
                 .IsDependentOn( "Create-Packages" )
                 .Does( () =>
                  {
