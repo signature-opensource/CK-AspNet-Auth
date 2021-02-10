782084547bb47fd34f682910947f80b2a1804e7f
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-02-10 12:33:47 +0100

    Calls setExpires on current.

--- Clients/webfrontauth/ts/src/AuthService.ts
+++ Clients/webfrontauth/ts/src/AuthService.ts
@@ -315,7 +315,8 @@ export class AuthService<T extends IUserInfo = IUserInfo> {
         // Keep the current rememberMe configuration: this is the "local" disconnect. 
         this._token = '';
         this._refreshable = false;
-        this._authenticationInfo = authInfo || this._typeSystem.authenticationInfo.none;
+        if( authInfo ) this._authenticationInfo = authInfo;
+        else this._authenticationInfo = this._authenticationInfo.setExpires();
         this.clearTimeouts();
         this.onChange();
     }
