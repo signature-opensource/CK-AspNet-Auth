dbd245eb11aed9d5add1a484bdb3c0cb68fc8c19
Dan Chiche <dan.chiche@signature-code.com>
2021-04-21 12:01:16 +0200

    Use ShouldSetToken in AuthInterceptor

--- Clients/webfrontauth-ngx/projects/webfrontauth-ngx/src/lib/AuthInterceptor.ts
+++ Clients/webfrontauth-ngx/projects/webfrontauth-ngx/src/lib/AuthInterceptor.ts
@@ -14,6 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
     public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(
             this.authService.token !== ''
+            && this.authService.shouldSetToken(request.url!)
                 ? request.clone({headers: request.headers.set('Authorization', 'Bearer ' + this.authService.token)})
                 : request
         );
