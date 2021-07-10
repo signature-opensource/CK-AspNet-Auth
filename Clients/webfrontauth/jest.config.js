e203276ca476c03fb2ba6fc0146403a90c328038
Kuinox <nvandeginste@gmail.com>
2021-07-10 14:45:22 +0200

    Dont try to even have fun once with JS tools.

--- Clients/webfrontauth/jest.config.js
+++ Clients/webfrontauth/jest.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: [ 'jest-localstorage-mock' ],
-  reporters: [
-    [
-      "jest-nyancat-reporter",
-      {
-        "suppressErrorReporter": false
-      }
-    ]
-  ]
+  setupFiles: [ 'jest-localstorage-mock' ]
 };
