8e0b6d9daa6ffb3107b5cc2ab6ce9225e03b017d
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-09-30 19:21:21 +0200

    WebFrontAuthRemoteAuthenticateAsync renamed.

--- CK.AspNet.Auth/RemoteAuthenticationEventsContextExtensions.cs
+++ CK.AspNet.Auth/RemoteAuthenticationEventsContextExtensions.cs
@@ -16,18 +16,29 @@ namespace CK.AspNet.Auth
     public static class RemoteAuthenticationEventsContextExtensions
     {
         /// <summary>
+        /// Obsolete.
+        /// </summary>
+        /// <typeparam name="TPayload">Type of the payload.</typeparam>
+        /// <param name="c">This ticket received context.</param>
+        /// <param name="payloadConfigurator">Action that must configure the payload.</param>
+        /// <returns>The awaitable.</returns>
+        [Obsolete( "Use WebFrontAuthOnTicketReceivedAsync (renaming).", error: false )]
+        public static Task WebFrontAuthRemoteAuthenticateAsync<TPayload>( this TicketReceivedContext c, Action<TPayload> payloadConfigurator )
+            => WebFrontAuthOnTicketReceivedAsync( c, payloadConfigurator );
+
+        /// <summary>
         /// Simple API used from <see cref="RemoteAuthenticationEvents.OnTicketReceived"/> to handle
-        /// external authentication: <see cref="WebFrontAuthService.HandleRemoteAuthentication{T}(TicketReceivedContext, Action{T})"/>
+        /// external authentication: <see cref="WebFrontAuthService.HandleRemoteAuthenticationAsync{T}(TicketReceivedContext, Action{T})"/>
         /// is called.
         /// </summary>
         /// <typeparam name="TPayload">Type of the payload.</typeparam>
         /// <param name="c">This ticket received context.</param>
         /// <param name="payloadConfigurator">Action that must configure the payload.</param>
         /// <returns>The awaitable.</returns>
-        public static Task WebFrontAuthRemoteAuthenticateAsync<TPayload>( this TicketReceivedContext c, Action<TPayload> payloadConfigurator )
+        public static Task WebFrontAuthOnTicketReceivedAsync<TPayload>( this TicketReceivedContext c, Action<TPayload> payloadConfigurator )
         {
             var authService = c.HttpContext.RequestServices.GetRequiredService<WebFrontAuthService>();
-            return authService.HandleRemoteAuthentication( c, payloadConfigurator );
+            return authService.HandleRemoteAuthenticationAsync( c, payloadConfigurator );
         }
 
         /// <summary>
@@ -36,15 +47,19 @@ namespace CK.AspNet.Auth
         /// (This method calls <see cref="HandleRequestContext{T}.HandleResponse()"/> that ends any further response processing.)
         /// </summary>
         /// <param name="f">This remote failure context.</param>
+        /// <param name="setUnsafeLevel">
+        /// True to downgrade the current authentication to <see cref="AuthLevel.Unsafe"/>.
+        /// By default the current authentication is kept as-is.
+        /// </param>
         /// <param name="errorId">
         /// Error identifier: should be a dotted identifier that could easily be used as a resource
         /// name (to map to translations in different languages).
         /// </param>
         /// <param name="errorText">When null, <see cref="RemoteFailureContext.Failure"/>'s <see cref="Exception.Message"/> is used.</param>
         /// <returns>The awaitable.</returns>
-        public static Task WebFrontAuthRemoteFailureAsync( this RemoteFailureContext f, string errorId = "RemoteFailure", string? errorText = null )
+        public static Task WebFrontAuthOnRemoteFailureAsync( this RemoteFailureContext f, bool setUnsafeLevel = false, string errorId = "RemoteFailure", string? errorText = null )
         {
-            return OnError( f, f.Properties, errorId, errorText ?? f.Failure.Message );
+            return OnError( f, f.Properties, setUnsafeLevel, errorId, errorText ?? f.Failure.Message );
         }
 
         /// <summary>
@@ -53,50 +68,54 @@ namespace CK.AspNet.Auth
         /// (This method calls <see cref="HandleRequestContext{T}.HandleResponse()"/> that ends any further response processing.)
         /// </summary>
         /// <param name="d">This remote failure context.</param>
+        /// <param name="setUnsafeLevel">
+        /// True to downgrade the current authentication to <see cref="AuthLevel.Unsafe"/>.
+        /// By default the current authentication is kept as-is.
+        /// </param>
         /// <param name="errorId">
         /// Error identifier: should be a dotted identifier that could easily be used as a resource
         /// name (to map to translations in different languages).
         /// </param>
         /// <param name="errorText">When null, <paramref name="errorId"/> is used.</param>
         /// <returns>The awaitable.</returns>
-        public static Task WebFrontAuthRemoteFailureAsync( this AccessDeniedContext d, string errorId = "AccessDenied", string? errorText = null )
+        public static Task WebFrontAuthOnAccessDeniedAsync( this AccessDeniedContext d, bool setUnsafeLevel = false, string errorId = "AccessDenied", string? errorText = null )
         {
-            return OnError( d, d.Properties, errorId, errorText ?? errorId );
+            return OnError( d, d.Properties, setUnsafeLevel, errorId, errorText ?? errorId );
         }
 
-        static Task OnError( HandleRequestContext<RemoteAuthenticationOptions> h, AuthenticationProperties properties, string errorId, string errorText )
+        static Task OnError( HandleRequestContext<RemoteAuthenticationOptions> h, AuthenticationProperties properties, bool setUnsafeLevel, string errorId, string errorText )
         {
             h.HandleResponse();
             var authService = h.HttpContext.RequestServices.GetRequiredService<WebFrontAuthService>();
-            WebFrontAuthHandler.ExtractClearWFAData( properties, out _, out var deviceId, out var initialScheme, out var returnUrl, out var callerOrigin );
-            return authService.SendRemoteAuthenticationError( h.HttpContext, deviceId, returnUrl, callerOrigin, errorId, errorText, initialScheme );
+            authService.GetWFAData( h.HttpContext, properties, out var fAuth, out var initialScheme, out var callerOrigin, out var returnUrl, out var userData );
+            if( setUnsafeLevel ) fAuth = fAuth.SetUnsafeLevel();
+            return authService.SendRemoteAuthenticationErrorAsync( h.HttpContext, fAuth, returnUrl, callerOrigin, errorId, errorText, initialScheme, h.Scheme.Name, userData );
         }
 
         /// <summary>
         /// Extracts the initial authentication from this context (from the "WFA-C" key of <see cref="RemoteFailureContext.Properties"/>).
         /// </summary>
-        /// <param name="this">This failure context.</param>
+        /// <param name="this">This ticket received context.</param>
         /// <returns>The initial authentication.</returns>
-        public static IAuthenticationInfo GetTicketAuthenticationInfo( this RemoteFailureContext @this ) => GetAuthenticationInfo( @this.HttpContext, @this.Properties );
+        public static IAuthenticationInfo GetTicketAuthenticationInfo( this TicketReceivedContext @this ) => GetFrontAuthenticationInfo( @this.HttpContext, @this.Properties ).Info;
 
         /// <summary>
         /// Extracts the initial authentication from this context (from the "WFA-C" key of <see cref="RemoteFailureContext.Properties"/>).
         /// </summary>
         /// <param name="this">This failure context.</param>
         /// <returns>The initial authentication.</returns>
-        public static IAuthenticationInfo GetTicketAuthenticationInfo( this AccessDeniedContext d ) => GetAuthenticationInfo( d.HttpContext, d.Properties );
+        public static IAuthenticationInfo GetTicketAuthenticationInfo( this RemoteFailureContext @this ) => GetFrontAuthenticationInfo( @this.HttpContext, @this.Properties ).Info;
 
-        static IAuthenticationInfo GetAuthenticationInfo( HttpContext h, AuthenticationProperties properties )
+        /// <summary>
+        /// Extracts the initial authentication from this context (from the "WFA-C" key of <see cref="RemoteFailureContext.Properties"/>).
+        /// </summary>
+        /// <param name="this">This failure context.</param>
+        /// <returns>The initial authentication.</returns>
+        public static IAuthenticationInfo GetTicketAuthenticationInfo( this AccessDeniedContext d ) => GetFrontAuthenticationInfo( d.HttpContext, d.Properties ).Info;
+
+        static FrontAuthenticationInfo GetFrontAuthenticationInfo( HttpContext httpContext, AuthenticationProperties properties )
         {
-            if( properties.Items.TryGetValue( "WFA-C", out var currentAuth ) )
-            {
-                var authService = h.RequestServices.GetRequiredService<WebFrontAuthService>();
-                return authService.UnprotectAuthenticationInfo( h, currentAuth ).Info;
-            }
-            WebFrontAuthHandler.ExtractClearWFAData( properties, out _, out var deviceId, out var _, out var _, out var _ );
-            var typeSystem = h.RequestServices.GetRequiredService<IAuthenticationTypeSystem>();
-            return typeSystem.AuthenticationInfo.Create( null, deviceId: deviceId );
+            return httpContext.RequestServices.GetRequiredService<WebFrontAuthService>().GetFrontAuthenticationInfo( httpContext, properties );
         }
-
     }
 }
