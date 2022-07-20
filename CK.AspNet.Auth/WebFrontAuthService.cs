0fa6f0b4006c8784fa80a075f0ffe0d8fe86f205
Olivier Spinelli <olivier.spinelli@signature-code.com>
2022-07-20 11:30:48 +0200

    Fixed NRT.

--- CK.AspNet.Auth/WebFrontAuthService.cs
+++ CK.AspNet.Auth/WebFrontAuthService.cs
@@ -891,7 +891,7 @@ namespace CK.AspNet.Auth
                                 bool raiseError = true;
                                 if( _autoCreateAccountService != null )
                                 {
-                                    UserLoginResult uAuto = await _autoCreateAccountService.CreateAccountAndLoginAsync( monitor, ctx );
+                                    UserLoginResult? uAuto = await _autoCreateAccountService.CreateAccountAndLoginAsync( monitor, ctx );
                                     if( uAuto != null )
                                     {
                                         raiseError = false;
