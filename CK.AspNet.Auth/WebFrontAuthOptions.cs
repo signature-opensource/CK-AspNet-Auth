8f18ca39af16745d65792ae779b56d0468868e16
Aymeric Richard <aymeric.richard@signature-code.com>
2023-08-31 15:21:24 +0200

    Cleanup.

--- CK.AspNet.Auth/WebFrontAuthOptions.cs
+++ CK.AspNet.Auth/WebFrontAuthOptions.cs
@@ -16,7 +16,7 @@ namespace CK.AspNet.Auth
         static readonly PathString _entryPath = new PathString( "/.webfront" );
 
         /// <summary>
-        /// The <see cref="WebFrontAuthService"/> is not designed to be added multiple 
+        /// The <see cref="WebFrontAuthService"/> is not designed to be added multiple
         /// times to an application, hence its name is unique.
         /// </summary>
         public const string OnlyAuthenticationScheme = "WebFrontAuth";
@@ -27,8 +27,8 @@ namespace CK.AspNet.Auth
         public PathString EntryPath => _entryPath;
 
         /// <summary>
-        /// Controls how much time the authentication will remain valid 
-        /// from the point it is created. 
+        /// Controls how much time the authentication will remain valid
+        /// from the point it is created.
         /// Defaults to 20 minutes.
         /// This time is extended if <see cref="SlidingExpirationTime"/> is set and
         /// when "<see cref="EntryPath"/>/c/refresh" is called.
@@ -38,8 +38,8 @@ namespace CK.AspNet.Auth
         public TimeSpan ExpireTimeSpan { get; set; } = TimeSpan.FromMinutes( 20 );
 
         /// <summary>
-        /// Controls how much time the long term, unsafe, authentication information 
-        /// will remain valid from the point it is created. 
+        /// Controls how much time the long term, unsafe, authentication information
+        /// will remain valid from the point it is created.
         /// Defaults to one year.
         /// This configuration can be changed dynamically.
         /// </summary>
@@ -80,7 +80,7 @@ namespace CK.AspNet.Auth
 
         /// <summary>
         /// Gets whether the authentication cookie (see <see cref="CookieMode"/>) requires or not https.
-        /// Note that the long term cookie uses <see cref="CookieOptions.Secure"/> sets to false since it 
+        /// Note that the long term cookie uses <see cref="CookieOptions.Secure"/> sets to false since it
         /// does not require any protection.
         /// Defaults to <see cref="CookieSecurePolicy.SameAsRequest"/>.
         /// This cannot be changed dynamically.
@@ -93,9 +93,9 @@ namespace CK.AspNet.Auth
         /// Defaults to <see cref="AuthenticationCookieMode.WebFrontPath"/>.
         /// </para>
         /// <para>
-        /// Setting it to <see cref="AuthenticationCookieMode.RootPath"/> should NOT BE used in 
+        /// Setting it to <see cref="AuthenticationCookieMode.RootPath"/> should NOT BE used in
         /// most cases: this mode, that is the same as the standard Cookie ASP.Net authentication,
-        /// is for standard and classical Web application. 
+        /// is for standard and classical Web application.
         /// </para>
         /// <para>
         /// Setting it to <see cref="AuthenticationCookieMode.None"/> disables all cookies: client apps
@@ -108,14 +108,14 @@ namespace CK.AspNet.Auth
         public AuthenticationCookieMode CookieMode { get; set; }
 
         /// <summary>
-        /// Gets or sets a list of available schemes returned for information from '/c/refresh' endpoint 
+        /// Gets or sets a list of available schemes returned for information from '/c/refresh' endpoint
         /// when 'schemes' appears in the query string.
         /// <para>
         /// Defaults to null: schemes are the same as <see cref="IWebFrontAuthLoginService.Providers"/>
         /// when this is null or empty.
         /// </para>
         /// <para>
-        /// When not null (or empty), this list takes precedence over the login service's providers: all supported 
+        /// When not null (or empty), this list takes precedence over the login service's providers: all supported
         /// schemes must be declared here (and unwanted ones must not appear).
         /// </para>
         /// <para>
@@ -130,12 +130,12 @@ namespace CK.AspNet.Auth
         public List<string>? AvailableSchemes { get; set; }
 
         /// <summary>
-        /// Gets or sets the refresh validation time. 
-        /// When set to other than <see cref="TimeSpan.Zero"/> the middleware will re-issue a new token 
-        /// (and new authentication cookie if <see cref="CookieMode"/> allows it) with a new expiration time any time it 
+        /// Gets or sets the refresh validation time.
+        /// When set to other than <see cref="TimeSpan.Zero"/> the middleware will re-issue a new token
+        /// (and new authentication cookie if <see cref="CookieMode"/> allows it) with a new expiration time any time it
         /// processes a ".webfront/c/refresh" request.
         /// <para>
-        /// This applies to <see cref="IAuthenticationInfo.Expires"/> but not to <see cref="IAuthenticationInfo.CriticalExpires"/>. 
+        /// This applies to <see cref="IAuthenticationInfo.Expires"/> but not to <see cref="IAuthenticationInfo.CriticalExpires"/>.
         /// This configuration can be changed dynamically: modifying the configuration will take the
         /// new value into account.
         /// </para>
@@ -185,8 +185,8 @@ namespace CK.AspNet.Auth
         /// <summary>
         /// Gets a mutable list of accepted returnUrl prefixes.
         /// <para>
-        /// The returnUrl optional parameter submitted to the '/c/startLogin' end point (case of an "inline login" based 
-        /// on page redirections rather that the recommended popup window) must exactly start with one of this 
+        /// The returnUrl optional parameter submitted to the '/c/startLogin' end point (case of an "inline login" based
+        /// on page redirections rather that the recommended popup window) must exactly start with one of this
         /// prefix (<see cref="StringComparison.Ordinal"/> is used) otherwise a 400 Bad Request error code is returned.
         /// </para>
         /// <para>
