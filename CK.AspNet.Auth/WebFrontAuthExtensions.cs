1d7c15a67d0ade4e7ffd1e48d47d1a13b9ceb3be
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-07-17 18:57:31 +0200

    WIP. A single CreateAspNetAuthServerAsync helper should be enough.

--- CK.AspNet.Auth/WebFrontAuthExtensions.cs
+++ CK.AspNet.Auth/WebFrontAuthExtensions.cs
@@ -1,10 +1,16 @@
+using CK.AspNet;
 using CK.AspNet.Auth;
 using CK.Auth;
+using CK.Core;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -14,19 +20,127 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class WebFrontAuthExtensions
     {
         /// <summary>
-        /// Adds the WebFrontAuth authentication services with options configuration.
-        /// This registers <see cref="IAuthenticationInfo"/> as a scoped dependency.
-        /// That relies on the <see cref="CK.AspNet.ScopedHttpContext"/> that MUST be registered.
+        /// Idempotent registration of the <see cref="WebFrontAuthService"/>, <see cref="IAuthenticationInfo"/> and <see cref="AuthenticationBuilder"/>
+        /// with the <see cref="WebFrontAuthOptions.OnlyAuthenticationScheme"/>.
+        /// <para>
+        /// When called more than once, all <paramref name="authOptions"/> are applied to the final <see cref="WebFrontAuthOptions"/>.
+        /// </para>
         /// </summary>
-        /// <param name="this">This Authentication builder.</param>
-        /// <param name="configure">Optional configuration action.</param>
-        /// <returns>This authentication builder to enable fluent syntax.</returns>
-        public static AuthenticationBuilder AddWebFrontAuth( this AuthenticationBuilder @this, Action<WebFrontAuthOptions>? configure = null )
+        /// <param name="builder">This builder.</param>
+        /// <param name="authOptions">Optional option configuration.</param>
+        /// <returns>This builder.</returns>
+        public static WebApplicationBuilder AddWebFrontAuth( this WebApplicationBuilder builder,
+                                                             Action<WebFrontAuthOptions>? authOptions = null )
         {
-            @this.Services.AddSingleton<WebFrontAuthService>();
-            @this.AddScheme<WebFrontAuthOptions, WebFrontAuthHandler>( WebFrontAuthOptions.OnlyAuthenticationScheme, "Web Front Authentication", configure );
-            @this.Services.TryAddScoped( sp => sp.GetRequiredService<CK.AspNet.ScopedHttpContext>().HttpContext.GetAuthenticationInfo() );
-            return @this;
+            var props = ((IHostApplicationBuilder)builder).Properties;
+            if( props.TryAdd( typeof( WebFrontAuthExtensions ), typeof( WebFrontAuthExtensions ) ) )
+            {
+                builder.Services.AddSingleton<WebFrontAuthService>();
+                builder.Services.AddScoped( sp => sp.GetRequiredService<ScopedHttpContext>().HttpContext.GetAuthenticationInfo() );
+                var authBuilder = builder.Services.AddAuthentication( WebFrontAuthOptions.OnlyAuthenticationScheme );
+                authBuilder.AddScheme<WebFrontAuthOptions, WebFrontAuthHandler>( WebFrontAuthOptions.OnlyAuthenticationScheme, "Web Front Authentication", authOptions );
+                builder.AppendApplicationBuilder( app => app.UseAuthentication() );
+            }
+            else if( authOptions != null )
+            {
+                // Already called. If an option configurator is present, register the new one.
+                // The OptionsFactory<T> will call all the registered IConfigureOptions<T>.
+                var configurator = new ConfigureNamedOptions<WebFrontAuthOptions>( WebFrontAuthOptions.OnlyAuthenticationScheme, authOptions );
+                builder.Services.AddSingleton<IConfigureOptions<WebFrontAuthOptions>>( configurator );
+            }
+            return builder;
+        }
+
+        /// <summary>
+        /// Add dangerous Cors support: this allows all orgigins, methods, headers AND supports credential.
+        /// This is unfortunately required in some testing scenario but should NEVER be used in production.
+        /// <para>
+        /// This method, just like <see cref="AddCors(WebApplicationBuilder, string)"/> and <see cref="AddCors(WebApplicationBuilder, Action{CorsPolicyBuilder})"/>
+        /// can be called multiple times: the last wins.
+        /// </para>
+        /// </summary>
+        /// <param name="builder">This builder.</param>
+        /// <returns>This builder.</returns>
+        public static WebApplicationBuilder AddUnsafeAllowAllCors( this WebApplicationBuilder builder )
+        {
+            return AddCors( builder, CorsAllowAllBuilder );
+
+            static void CorsAllowAllBuilder( CorsPolicyBuilder o )
+            {
+                o.AllowAnyMethod().AllowCredentials().AllowAnyHeader().SetIsOriginAllowed( _ => true );
+            }
+        }
+
+        /// <summary>
+        /// Add Cors support for a single policy.
+        /// <para>
+        /// This method, just like <see cref="AddCors(WebApplicationBuilder, string)"/> and <see cref="AddUnsafeAllowAllCors(WebApplicationBuilder)"/>
+        /// can be called multiple times: the last wins.
+        /// </para>
+        /// </summary>
+        /// <param name="builder">This builder.</param>
+        /// <param name="policyBuilder">The cors policy builder.</param>
+        /// <returns></returns>
+        public static WebApplicationBuilder AddCors( this WebApplicationBuilder builder,
+                                                     Action<CorsPolicyBuilder> policyBuilder )
+        {
+            Throw.CheckNotNullArgument( policyBuilder );
+            var props = ((IHostApplicationBuilder)builder).Properties;
+            if( !props.TryGetValue( typeof( CorsPolicyBuilder ), out var currentPolicy ) )
+            {
+                props.Add( typeof( CorsPolicyBuilder ), policyBuilder );
+                builder.Services.AddCors();
+                builder.AppendApplicationBuilder( DoUseCors( props ) );
+            }
+            else
+            {
+                props[typeof( CorsPolicyBuilder )] = policyBuilder;
+            }
+            return builder;
+
+        }
+
+        /// <summary>
+        /// Add Cors support for a named policy. This method can be called multiple times: the last <paramref name="policyName"/>  wins.
+        /// <para>
+        /// Named policy must be defined by using <see cref="CorsServiceCollectionExtensions.AddCors(IServiceCollection, Action{CorsOptions})"/>
+        /// and configuring the <see cref="CorsOptions"/>.
+        /// </para>
+        /// <para>
+        /// This method, just like <see cref="AddCors(WebApplicationBuilder, Action{CorsPolicyBuilder})"/> and <see cref="AddUnsafeAllowAllCors(WebApplicationBuilder)"/>
+        /// can be called multiple times: the last wins.
+        /// </para>
+        /// </summary>
+        /// <param name="builder">This builder.</param>
+        /// <param name="policyName">The policy name.</param>
+        /// <returns></returns>
+        public static WebApplicationBuilder AddCors( this WebApplicationBuilder builder,
+                                                     string policyName )
+        {
+            Throw.CheckNotNullOrWhiteSpaceArgument( policyName );
+            var props = ((IHostApplicationBuilder)builder).Properties;
+            if( !props.TryGetValue( typeof( CorsPolicyBuilder ), out var currentPolicy ) )
+            {
+                props.Add( typeof( CorsPolicyBuilder ), policyName );
+                builder.Services.AddCors();
+                builder.AppendApplicationBuilder( DoUseCors( props ) );
+            }
+            else
+            {
+                props[typeof( CorsPolicyBuilder )] = policyName;
+            }
+            return builder;
+        }
+
+        static Action<IApplicationBuilder> DoUseCors( IDictionary<object, object> props )
+        {
+            return app =>
+            {
+                var p = props[typeof( CorsPolicyBuilder )];
+                if( p is string name ) app.UseCors( name );
+                else app.UseCors( (Action<CorsPolicyBuilder>)p );
+                props.Remove( typeof( CorsPolicyBuilder );
+            };
         }
     }
 }
--- CK.Testing.AspNetServer.Auth/AspNetAuthServerTestHelperExtensions.cs
+++ CK.Testing.AspNetServer.Auth/AspNetAuthServerTestHelperExtensions.cs
@@ -2,12 +2,15 @@ using CK.AspNet.Auth;
 using CK.Auth;
 using CK.Core;
 using CK.Setup;
+using FluentAssertions.Common;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using static CK.Testing.MonitorTestHelper;
 
@@ -21,119 +24,46 @@ namespace CK.Testing
     public static class AspNetAuthServerTestHelperExtensions
     {
         /// <summary>
-        /// Creates, configures and starts a <see cref="RunningAspNetServer"/> that supports authentication.
-        /// <para>
-        /// This is based on <see cref="AspNetServerTestHelperExtensions.CreateMinimalAspNetServerAsync(IMonitorTestHelper, Action{IServiceCollection}?, Action{IApplicationBuilder}?)"/>
-        /// and no services or middlewares other than CORS support (configured without any restriction) and authentication support with the
-        /// <see cref="WebFrontAuthService"/> is added.
-        /// </para>
-        /// <para>
-        /// The <see cref="CreateAspNetAuthServerAsync(IStObjMap, Action{IServiceCollection}?, Action{AuthenticationBuilder}?, Action{IApplicationBuilder}?, Action{WebFrontAuthOptions}?)"/>
-        /// extension method is easier to use as the CKomposable takes care of all the required automatic services and handles the <see cref="FakeWebFrontAuthLoginService"/>
-        /// and <see cref="FakeUserDatabase"/> if needed.
-        /// </para>
-        /// </summary>
-        /// <param name="helper">This helper.</param>
-        /// <param name="configureServices">Application services configurator must at least provide a <see cref="WebFrontAuthService"/> implementation.</param>
-        /// <param name="configureAuth">Optional authentication configurator.</param>
-        /// <param name="configureApplication">Optional application configurator.</param>
-        /// <param name="authOptions">Optional authentication options configurator.</param>
-        /// <returns>A running Asp.NET server with authentication support.</returns>
-        public static Task<RunningAspNetServer> CreateAspNetAuthServerAsync( this IMonitorTestHelper helper,
-                                                                             Action<IServiceCollection> configureServices,
-                                                                             Action<AuthenticationBuilder>? configureAuth = null,
-                                                                             Action<IApplicationBuilder>? configureApplication = null,
-                                                                             Action<WebFrontAuthOptions>? authOptions = null )
-        {
-            Throw.CheckNotNullArgument( configureServices );
-
-            static void ConfigureServices( IServiceCollection services,
-                                           Action<IServiceCollection>? configureServices,
-                                           Action<AuthenticationBuilder>? configureAuth,
-                                           Action<WebFrontAuthOptions>? webFrontAuthOptions )
-            {
-                services.AddCors();
-                var authBuilder = services.AddAuthentication( WebFrontAuthOptions.OnlyAuthenticationScheme )
-                                          .AddWebFrontAuth( webFrontAuthOptions );
-                configureAuth?.Invoke( authBuilder );
-                configureServices?.Invoke( services );
-            }
-
-            static void ConfigureApplication( IApplicationBuilder app, Action<IApplicationBuilder>? configureApplication )
-            {
-                app.UseCors( o => o.AllowAnyMethod().AllowCredentials().AllowAnyHeader().SetIsOriginAllowed( _ => true ) );
-                app.UseAuthentication();
-                configureApplication?.Invoke( app );
-            }
-
-            return helper.CreateMinimalAspNetServerAsync( configureServices: services => ConfigureServices( services, configureServices, configureAuth, authOptions ),
-                                                          configureApplication: app => ConfigureApplication( app, configureApplication ) );
-        }
-
-        /// <summary>
-        /// Creates, configures and starts a <see cref="RunningAspNetServer"/> that supports authentication.
+        /// Creates, configures and starts a <see cref="RunningAspNetServer"/> that supports authentication with
+        /// <see cref="WebFrontAuthExtensions.AddUnsafeAllowAllCors(WebApplicationBuilder)"/> (this is for tests only).
         /// <para>
         /// If this <see cref="IStObjMap"/> doesn't have a <see cref="IWebFrontAuthLoginService"/> implementation,
         /// the <see cref="FakeWebFrontAuthLoginService"/> is automatically registered and if no <see cref="IUserInfoProvider"/>
         /// exists, the <see cref="FakeUserDatabase"/> is automatically registered.
         /// </para>
         /// </summary>
+        /// Note: <c>IUserInfoProvider</c> and <c>IWebFrontAuthLoginService</c> must obviously be coupled somehow. ISP principle
+        /// made us separate the 2 concerns but implementations should be coherent. This cannot be challenged
+        /// here (and in a way it shouldn't be).
+        /// <remarks>
+        /// </remarks>
         /// <param name="map">This StObjMap.</param>
-        /// <param name="configureServices">Optional application services configurator.</param>
-        /// <param name="configureAuth">Optional authentication configurator.</param>
-        /// <param name="configureApplication">Optional application configurator.</param>
         /// <param name="authOptions">
         /// Optional authentication options configurator.
         /// By default <see cref="WebFrontAuthOptions.SlidingExpirationTime"/> is set to 10 minutes.
         /// </param>
+        /// <param name="configureApplication">Optional application configurator.</param>
         /// <returns>A running Asp.NET server with authentication support.</returns>
-        public static Task<RunningAspNetServer> CreateAspNetAuthServerAsync( this IStObjMap map,
-                                                                             Action<IServiceCollection>? configureServices = null,
-                                                                             Action<AuthenticationBuilder>? configureAuth = null,
-                                                                             Action<IApplicationBuilder>? configureApplication = null,
-                                                                             Action<WebFrontAuthOptions>? authOptions = null )
+        public static Task<RunningAspNetServer> CreateRunningAspNetAuthServerAsync( this WebApplicationBuilder builder,
+                                                                                    IStObjMap map,
+                                                                                    Action<WebFrontAuthOptions>? authOptions = null,
+                                                                                    Action<IApplicationBuilder>? configureApplication = null )
         {
-            static void ConfigureServices( IServiceCollection services,
-                                           IStObjMap map,
-                                           Action<IServiceCollection>? configureServices )
+            if( !map.Services.Mappings.ContainsKey( typeof( IUserInfoProvider ) ) )
             {
-                configureServices?.Invoke( services );
-                // It is not possible to transfer the "Fake" handling to the IMonitorTestHelper.CreateAspNetAuthServerAsync method
-                // by doing this after the call to the configureServices:
-                //
-                //   services.TryAddSingleton<IUserInfoProvider, FakeUserDatabase>();
-                //   services.TryAddSingleton<IWebFrontAuthLoginService, FakeWebFrontAuthLoginService>();
-                //
-                // Because the CKomposable map needs to see all the services: it locks the ServiceCollections.
-                // We only handles the "Fakes" in this overload.
-                //
-                // Note: IUserInfoProvider and IWebFrontAuthLoginService must obviously be coupled somehow. ISP principle
-                //       made us separate the 2 concerns but implementations should be coherent but this cannot be challenged
-                //       here (and in a way it shouldn't be).
-                //
-                if( !map.Services.Mappings.ContainsKey( typeof( IUserInfoProvider ) ) )
-                {
-                    // Use TryAdd to allow the configureServices function to inject its IUserInfoProvider
-                    // if the CKomposable map is missing it.
-                    services.TryAddSingleton<IUserInfoProvider, FakeUserDatabase>();
-                }
-                if( !map.Services.Mappings.ContainsKey( typeof( IWebFrontAuthLoginService ) ) )
-                {
-                    // Use TryAdd to allow the configureServices function to inject its IWebFrontAuthLoginService
-                    // if the CKomposable map is missing it.
-                    services.TryAddSingleton<IWebFrontAuthLoginService, FakeWebFrontAuthLoginService>();
-                }
-                // If the map contains a IUserInfoProvider or a IWebFrontAuthLoginService they will win as they will
-                // be the last added.
-                services.AddStObjMap( TestHelper.Monitor, map );
+                // Use TryAdd to allow the configureServices function to inject its IUserInfoProvider
+                // if the CKomposable map is missing it.
+                builder.Services.TryAddSingleton<IUserInfoProvider, FakeUserDatabase>();
             }
-
-            return TestHelper.CreateAspNetAuthServerAsync(
-                configureServices: services => ConfigureServices( services, map, configureServices ),
-                configureAuth: configureAuth,
-                configureApplication: configureApplication,
-                authOptions: authOptions );
-
+            if( !map.Services.Mappings.ContainsKey( typeof( IWebFrontAuthLoginService ) ) )
+            {
+                // Use TryAdd to allow the configureServices function to inject its IWebFrontAuthLoginService
+                // if the CKomposable map is missing it.
+                builder.Services.TryAddSingleton<IWebFrontAuthLoginService, FakeWebFrontAuthLoginService>();
+            }
+            builder.AddWebFrontAuth( authOptions );
+            builder.AddUnsafeAllowAllCors();
+            return builder.CreateRunningAspNetServerAsync( configureApplication );
         }
 
     }
