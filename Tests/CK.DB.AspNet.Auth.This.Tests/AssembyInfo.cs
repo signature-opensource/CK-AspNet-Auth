46252cd3a3a5be46ff22fec9e8be3aa0bdd4a5be
Arthur Cheng <arthur.cheng@signature.one>
2026-04-14 15:29:34 +0200

    removed CK.DB.AspNet.Auth and its test project (moved into its own repository https://github.com/signature-opensource/ck-db-aspnet-auth)

--- Tests/CK.DB.AspNet.Auth.This.Tests/AssembyInfo.cs
+++
@@ -1,3 +0,0 @@
-
-[assembly: CK.Setup.PreserveAssemblyReference( typeof( CK.DB.User.UserPassword.Package ) )]
-[assembly: CK.Setup.PreserveAssemblyReference( typeof( CK.DB.User.UserGoogle.Package ) )]
